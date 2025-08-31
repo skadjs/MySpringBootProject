efea2778a3d8c61671813ca061768d66724c8ab9
NamEon <143885031+skadjs@users.noreply.github.com>
2025-09-01 01:36:37 +0900

    commit

--- src/main/java/com/rookies4/myspringboot/MySpringBoot3ProjectApplication.java
+++ src/main/java/com/rookies4/myspringboot/MySpringBoot3ProjectApplication.java
@@ -1,24 +1,33 @@
 package com.rookies4.myspringboot;
 
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//Application 클래스가 Configuration(설정) 클래스 역할을 한다. @SpringBootConfiguration
-//반드시 해당 클래스를 작성할 때 base-package 아래에 작성해야 한다. @ComponentScan
-//외부 라이브러리에 대한 설정 기능을 제공하는 Autoconfiguration을 활성화해 주는 역할. @EnableAutoConfiguration
 public class MySpringBoot3ProjectApplication {
 
 	public static void main(String[] args) {
+
+		//SpringApplication.run(MySpringBoot3ProjectApplication.class, args);
 		SpringApplication application = new SpringApplication(MySpringBoot3ProjectApplication.class);
+		//Application 타입을 변경하기
 		application.setWebApplicationType(WebApplicationType.SERVLET);
-		application.run();
+		application.run(args);
 	}
 
 	@Bean
 	public String hello() {
 		return "Hello SpringBoot";
 	}
+
+	@Bean
+	Hibernate6Module hibernate6Module() {
+		Hibernate6Module hibernate6Module = new Hibernate6Module();
+		hibernate6Module.configure(Hibernate6Module.Feature.FORCE_LAZY_LOADING,true);
+		return hibernate6Module;
+	}
+
 }
---
+++ src/main/java/com/rookies4/myspringboot/entity/Student.java
@@ -0,0 +1,46 @@
+package com.rookies4.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Student
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    //PK (Sequential Value)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long id;
+
+    //이름
+    @Column(nullable = false)
+    private String name;
+
+    //학번
+    @Column(unique = true, nullable = false)
+    private String studentNumber;
+
+    /*
+        양방향관계 Student에서 StudentDetail을 참조할 수 있도록
+        FK에 해당하는 필드명을 mappedBy에 설정한다.*
+     */
+    //1:1관계 지연로딩
+    @OneToOne(fetch = FetchType.LAZY,
+            mappedBy = "student",
+            cascade = CascadeType.ALL)
+    private StudentDetail studentDetail;
+
+    //N:1 Student:Department 관계에서 N쪽에 해당하는 Student가 Owner
+    //department변수는 테이블의 FK와 매핑되는 필드임.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
