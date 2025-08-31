247ca9c7141a483d1937957c30e435fb774e10ed
NamEon <143885031+skadjs@users.noreply.github.com>
2025-08-31 22:11:34 +0900

    commit

--- src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
+++ src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
@@ -1,5 +1,7 @@
 package com.rookies4.myspringboot.runner;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -11,8 +13,20 @@ public class MyPropsRunner implements ApplicationRunner {
         System.out.println("VM Arguments = " + args.containsOption("foo"));
         System.out.println("Program Arguments = " + args.containsOption("bar"));
 
+        // Program Argument의 모든 이름을 출력하기
         for (String argName : args.getOptionNames()) {
-            System.out.println("argName = " + argName);
+            System.out.println("아규먼트 이름 = " + argName);
         }
+
+        // args.getOptionNames()의 리턴타입: Set<String>
+        // Iterable의 foreach() 메서드 호출하기
+        // Consumer의 추상메서드 void accept(T t)
+        // 1. 익명의 Inner class (Anonymous Inner Class)
+        args.getOptionNames().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("Inner Class 아규먼트 이름 = " + s);
+            }
+        });
     }
 }
